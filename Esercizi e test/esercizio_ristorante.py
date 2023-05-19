e2336aa346535588ab40dca46f12c510240a513c
LeoPagani <133334693+pagus93@users.noreply.github.com>
2023-05-19 11:48:47 +0200

    Add files via upload

---
+++ Esercizi e test/esercizio_ristorante.py
@@ -0,0 +1,45 @@
+
+menu = dict()
+ordine = dict()
+flag = True
+
+while flag:
+    scelta = input("""Editor menu e ordini:
+1. aggiungi un piatto al menu
+2. cancella un piatto dal menu
+3. visualizza il menu completo
+4. aggiungi un ordine
+5. calcola il conto
+0. esci
+""")
+
+    if scelta == '1':
+        nome = input("Inserire il nome del piatto da aggiungere: ")
+        prezzo = input("Inserire il prezzo (in €): ")
+        menu[nome] = prezzo
+
+    elif scelta == '2':
+        nome = input("Inserire il nome del piatto da cancellare: ")
+        del menu[nome]
+       
+    elif scelta == '3':
+        print(menu)
+
+    elif scelta == '4':
+        nome = input("Inserire il nome del piatto: ")
+        quantita = input("Inserire la quantità ordinata: ")
+        ordine[nome] = quantita
+    
+    elif scelta == '5':
+        conto = 0
+        for nome in ordine.keys():
+            prezzo = float(menu[nome])
+            quantita = float(ordine[nome])
+            conto += prezzo*quantita
+        print("Il conto è di", conto, "€")
+
+    elif scelta == '0':
+        flag = False
+
+    else:
+        print("Errore: comando non riconosciuto")
